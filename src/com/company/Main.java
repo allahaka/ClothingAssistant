cd46ee09cbc796270d9955c446a31c368fa255be
allahaka <wikczerski@gmail.com>
2022-02-02 16:46:14 +0100

    providing API keys in a somewhat secure way

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static String GEO_DECODE_API_KEY;
+    public static String OPEN_WEATHER_API_KEY;
+    public static List<Location> LocationsList;
+
+    public static void main(String @NotNull [] args){
+        Main.GEO_DECODE_API_KEY = args[0];
+        Main.OPEN_WEATHER_API_KEY = args[1];
         Menu menu = new Menu();
         menu.start("");
     }
